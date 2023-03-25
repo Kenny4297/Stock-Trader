d5e9570b913a178439d6345e9141f111e909781b
Kenny Cordero <geckob4i@gmail.com>
2023-03-25 11:35:38 -0500

    added instructions, layout structure as comments, created Readme

--- Main.py
+++ Main.py
@@ -1,1 +1,29 @@
-print("git testing!")
+# Create A stock trader application that follows the instructions in 'instructions.txt'. 
+
+# What I will need:
+# USER CLASS
+# Dictionary to store stocks
+# Wallet to keep track of Users cash
+# Buying and selling options
+    # If buy, ask user how many stocks they want to buy
+
+
+# STOCKS CLASS 
+# Contain all the stocks that are available to buy using the 'stock list' array in 'list_of_companies'. 
+# Keeps track of each updated stock each day
+        
+
+
+
+
+
+
+
+
+
+def Main():
+    pass
+
+
+if __name__ == '_main_':
+    Main()
